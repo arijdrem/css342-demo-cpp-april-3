d03f365b202b33a572cff7506388d0c2c737e3e0
student <btanvir@uw.edu>
2025-04-04 23:09:33 -0700

    Add isPrime function and tests

--- main.cpp
+++ main.cpp
@@ -60,7 +60,47 @@ void test_isPowerOfTwo() {
     cout << (pass ? "PASS" : "FAIL") << endl;
 }
 
+/*
+ * Function: isPrime
+ * Returns true if n is a prime number.
+ * I wrote this using the logic I remember from Java.
+ * Basically checking if anything divides n from 2 up to n-1.
+ */
+bool isPrime(int n) {
+    if (n <= 1) return false; // 0, 1, and negatives are not prime
+
+    for (int i = 2; i < n; ++i) {
+        if (n % i == 0) {
+            return false; // divisible by something other than 1 and itself
+        }
+    }
+
+    return true;
+}
+
+/*
+ * Function: test_isPrime
+ * Runs test cases on the isPrime function and prints PASS or FAIL.
+ */
+void test_isPrime() {
+    bool pass = true;
+
+    pass &= isPrime(2) == true;
+    pass &= isPrime(3) == true;
+    pass &= isPrime(4) == false;
+    pass &= isPrime(5) == true;
+    pass &= isPrime(9) == false;
+    pass &= isPrime(1) == false;
+    pass &= isPrime(0) == false;
+    pass &= isPrime(-3) == false;
+
+    cout << (pass ? "PASS" : "FAIL") << endl;
+}
+
 int main() {
     test_isPowerOfTwo();
+    test_isPrime();
+    // reduceFraction test will go here next
     return 0;
 }
+
