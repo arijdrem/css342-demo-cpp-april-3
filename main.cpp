349f518971ae37c6403255766a7599b882172272
student <btanvir@uw.edu>
2025-04-04 22:37:21 -0700

    Powerof2 HW

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+bool isPowerOfTwo_loop(int n) {
+    // To be implemented
+    return false;
+}
+
+bool isPowerOfTwo_bitwise(int n) {
+    // To be implemented
+    return false;
+}
+
+bool isPrime(int n) {
+    // To be implemented
+    return false;
+}
+
+void reduceFraction(int& numerator, int& denominator) {
+    // To be implemented
+}
+
+void runTests() {
+    // Here we will add test cases
+}
+
+int main() {
+    runTests();
+    return 0;
+}
